86a6e24380fa9160ed3649adbc90e527d0c1b05f
Vitaliy Tkachenko <vk@leev.in.ua>
2015-06-23 23:29:07 +0300

    Добавлен views\ex\error.php Изменен views\layouts\main.php Удален views по умолчанию views\site

--- views/ex/error.php
+++ views/ex/error.php
@@ -9,19 +9,12 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
+<div class="ex-error">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+        <?= nl2br(Html::encode($exception->getMessage())) ?>
     </div>
-
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
 
 </div>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,24 +26,31 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Ex Media Search',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
                 ],
             ]);
+            $itemsAcion = [];
+            $itemsAcion[] = ['label' => 'Главная', 'url' => ['/ex/home']];
+            $itemsAcion[] = ['label' => 'Фильмы', 'url' => ['/ex/films']];
+            $itemsAcion[] = ['label' => 'Сериалы', 'url' => ['/ex/serials']];
+            $itemsAcion[] = ['label' => 'Mузыка', 'url' => ['/ex/music']];
+            // доступно только для аминистратора            
+            if(Yii::$app->user->can('admin')){
+               $itemsAcion[] = ['label' => 'Генератор', 'url' => ['/ex/generator']];            
+            }
+            $itemsAcion[] = Yii::$app->user->isGuest ?
+                        ['label' => 'Авторизация', 
+				    'url' => ['/ex/login']] :
+                        ['label' => 'Выход (' . Yii::$app->user->identity->username . ')',
+                        	    'url' => ['/ex/logout'],
+                                   'linkOptions' => ['data-method' => 'post']];
+            
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
-                'items' => [
-                    ['label' => 'Home', 'url' => ['/site/index']],
-                    ['label' => 'About', 'url' => ['/site/about']],
-                    ['label' => 'Contact', 'url' => ['/site/contact']],
-                    Yii::$app->user->isGuest ?
-                        ['label' => 'Login', 'url' => ['/site/login']] :
-                        ['label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                            'url' => ['/site/logout'],
-                            'linkOptions' => ['data-method' => 'post']],
-                ],
+                'items' => $itemsAcion,
             ]);
             NavBar::end();
         ?>
@@ -58,8 +65,8 @@ AppAsset::register($this);
 
     <footer class="footer">
         <div class="container">
-            <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-            <p class="pull-right"><?= Yii::powered() ?></p>
+            <p class="pull-left">&copy; Ex Media Search <?= date('Y') ?></p>
+            <p class="pull-right">&nbsp;</p>
         </div>
     </footer>
 
