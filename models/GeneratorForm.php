306ea0b026e6a2e16d3779d471f46fc31c849f72
Vitaliy Tkachenko <vk@leev.in.ua>
2015-06-24 02:29:35 +0300

    изменение set_error_handler         set_error_handler(             create_function(                 '$c, $m, $f, $l',                 'throw new ErrorException($errstr, $errno, 0, $errfile, $errline);'             ),-1         );

--- models/GeneratorForm.php
+++ models/GeneratorForm.php
@@ -75,10 +75,12 @@ class GeneratorForm extends Model {
     private function getCategory() {
         $category = [];
         //set_error_handler(create_function('$c, $m, $f, $l', 'return true;'), -1);
-        function exception_error_handler($errno, $errstr, $errfile, $errline ) {
-            throw new ErrorException($errstr, $errno, 0, $errfile, $errline);
-        }
-        set_error_handler("exception_error_handler");
+        set_error_handler(
+            create_function(
+                '$c, $m, $f, $l',
+                'throw new ErrorException($errstr, $errno, 0, $errfile, $errline);'
+            ),-1
+        );
         try {
             $cache = $this->cacheFileInit();
             $content = $cache->get($this->urlCategory);
