1ec6a1a29740e7873c121489c73beac9adbd3a9b
Vitaliy Tkachenko <vk@leev.in.ua>
2015-06-24 22:40:03 +0300

    Настройка параметров соединения с базой данных (master,slave)

--- config/db.php
+++ config/db.php
@@ -1,9 +1,36 @@
 <?php
-
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+
+    // common configuration for masters
+    'masterConfig' => [
+        'username' => 'master',
+        'password' => 'PUSHvJAdDdXFs7nx',
+        'attributes' => [
+            // use a smaller connection timeout
+            PDO::ATTR_TIMEOUT => 3,
+        ],
+        'charset' => 'utf8',
+    ],
+
+    // list of master configurations
+    'masters' => [
+        ['dsn' => 'mysql:host=localhost;dbname=yii2_ex'],
+    ],
+
+    // common configuration for slaves
+    'slaveConfig' => [
+        'username' => 'slave',
+        'password' => 'UfzL4XQTVyQa86xu',
+        'attributes' => [
+            // use a smaller connection timeout
+            PDO::ATTR_TIMEOUT => 3,
+        ],
+        'charset' => 'utf8',
+    ],
+
+    // list of slave configurations
+    'slaves' => [
+        ['dsn' => 'mysql:host=localhost;dbname=yii2_ex'],
+    ],
 ];
